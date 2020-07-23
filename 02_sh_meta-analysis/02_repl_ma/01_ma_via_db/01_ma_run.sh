4d1b9e6c38b89d50eb57a536c959a38547ef6995
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2020-07-23 15:57:25 +0700

    Added scripts for MA Replication and pre-meta QC using GWAS-MAP

---
+++ 02_sh_meta-analysis/02_repl_ma/01_ma_via_db/01_ma_run.sh
@@ -0,0 +1,7 @@
+# Aim of this script is to run MA Replication of Shared heredity using GWAS-MAP
+
+out="/mnt/polyomica/projects/bp-sh/data/02_sh_ma/02_repl_ma/01_ma_via_db/"
+
+run_meta_analysis --gwas-ids 26,33,40 --genomic-control ON --output-folder "$out" --output-filename SH_MA_REPL.csv
+
+
---
+++ 02_sh_meta-analysis/02_repl_ma/01_ma_via_db/02_pre-meta_qc.sh
@@ -0,0 +1,7 @@
+# Aim of this script is to pre-meta QC of MA Replication Shared heredity using GWAS-MAP
+
+out="/mnt/polyomica/projects/bp-sh/data/02_sh_ma/02_repl_ma/01_ma_via_db/"
+
+run_pre_meta_qc_report --gwas-ids 26,33,40 --output-dir "$out" --output-file SH_MA_REPL
+
+
