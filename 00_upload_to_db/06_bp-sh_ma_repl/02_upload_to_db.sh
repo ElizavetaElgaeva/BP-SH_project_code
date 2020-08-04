37059bdb0437e49bb0f621053744f1a28732c7a2
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2020-08-05 00:26:23 +0700

    Added scripts for unification and uploading of BP-SH MA Replication

---
+++ 00_upload_to_db/06_bp-sh_ma_repl/02_upload_to_db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Aim of this script is to upload Replication meta-analysis of Back pain without Shared Heredity trait to GWAS-MAP
+
+run_upload --gwas-path=/mnt/polyomica/projects/bp-sh/data/00_upload_to_db/06_bp-sh_ma_repl/unification_results/BP-SH_ma_repl_output_done.csv
+
+
