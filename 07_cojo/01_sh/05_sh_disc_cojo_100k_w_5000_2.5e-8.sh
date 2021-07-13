44f2795eeb801492036be726c48c2e1ff38d4cd7
Elizaveta Elgaeva <eelgaeva@gaussian.polyomica.com>
2021-07-13 16:04:33 +0200

    Added scripts for COJO, corrected threshold

--- 07_cojo/01_sh/05_sh_disc_cojo_100k_w_5000_2.5e-8.sh
+++ 07_cojo/01_sh/05_sh_disc_cojo_100k_w_5000_2.5e-8.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Aim of this script is to run COJO on SH Discovery data with p = 1e-05 threshold
+# Aim of this script is to run COJO on SH Discovery data with p = 2.5e-08 threshold
 
 path='/storage/projects/PainOmics/bp-sh/data/cojo/01_sh/'
 
@@ -9,9 +9,9 @@ do
    --bfile /storage/projects/PainOmics/MV_GWAS/100k_bed_filtered/100k_chr"$i" \
    --maf 0.0002 \
    --cojo-slct \
-   --cojo-p 1e-5 \
+   --cojo-p 2.5e-08 \
    --chr $i \
    --cojo-wind 5000 \
    --cojo-file ${path}/input/SH_disc_output_done.for_cojo \
-   --out ${path}/output/discovery/1e-5/sh_disc_chr"$i"_1e-5.cojo
+   --out ${path}/output/discovery/2.5e-8/sh_disc_chr"$i"_2.5e-8.cojo
 done
---
+++ 07_cojo/01_sh/06_sh_ma_eur_cojo_100k_w_5000_2.5e-8.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Aim of this script is to run COJO on SH MA Eur data with p = 2.5e-08 threshold
+
+path='/storage/projects/PainOmics/bp-sh/data/cojo/01_sh/'
+
+for i in {1..22}
+do
+   /storage/projects/PainOmics/BP_crude_GWAS/gcta_1.90.0beta/gcta64 \
+   --bfile /storage/projects/PainOmics/MV_GWAS/100k_bed_filtered/100k_chr"$i" \
+   --maf 0.0002 \
+   --cojo-slct \
+   --cojo-p 2.5e-8 \
+   --chr $i \
+   --cojo-wind 5000 \
+   --cojo-file ${path}/input/SH_ma_eur_output_done.for_cojo \
+   --out ${path}/output/2.5e-8/sh_ma_eur_chr"$i"_2.5e-8.cojo
+done
