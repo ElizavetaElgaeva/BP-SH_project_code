9601798529eb9f558d493eb5d94af6cfc4fc2a30
DmitryVerzun <79205051+DmitryVerzun@users.noreply.github.com>
2021-07-17 10:02:37 +0300

    Create 2.5e-8.sh

---
+++ 08_bp-sh_ma_func_an/DEPICT/2.5e-8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+### The aim of this script is to run DEPICT for meta-analysis cohort for BP-SH COJO results with p-value treshold 2.5e-08
+
+### Config file parameters:
+
+#analysis_path: /mnt/polyomica/projects/bp-sh/data/08_bp-sh_ma_func_an/depict/2.5e-8
+
+#gwas_summary_statistics_file: /mnt/polyomica/projects/bp-sh/data/07_cojo/02_bp-sh/bp-sh_ma_eur_all_chr_2.5e-8.jma.cojo
+
+#plink_executable: /usr/local/bin/plink
+
+#association_pvalue_cutoff:  1
+
+/home/common/projects/depict_software/DEPICT_v194/src/python/depict.py /mnt/polyomica/projects/bp-sh/data/08_bp-sh_ma_func_an/depict/2.5e-8/bp-sh_ma_eur_all_chr.cfg
