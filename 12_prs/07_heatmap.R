a2526e6313004b046d83fbb3e013d69fa9b3f10d
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2021-09-21 22:39:16 +0700

    Corrected heatmap visualization

--- 12_prs/07_heatmap.R
+++ 12_prs/07_heatmap.R
@@ -9,18 +9,22 @@ tab <- fread("glm_results_extended_desc.txt")
 colnames(tab)
 
 pp <- tab[, c("bp_p", "sh_p", "bp-sh_p")]
+thr <- 3.646973e-05
+nonsig <- which(pp > thr)
 
-traits <- tab$"code"
+traits <- tab$"description"
 traits[20:25] <- c("Chronic back pain", "Chronic neck pain", "Chronic hip pain", "Chronic stomach pain", "Chronic knee pain", "Chronic headache")
 tab <- tab[, c("bp_beta", "sh_beta", "bp-sh_beta")]
 tab <- as.matrix(tab)
 rownames(tab) <- traits
 colnames(tab) <- c("CBP PRS", "SGIT PRS", "CBP UGIT PRS")
 
-pdf("heatmap.pdf", width = 30, height = 30)
-heatmap.2(as.matrix(tab), cellnote = round(tab, digits = 2),
-	  notecol = "black", col = redblue(75),
-	  margins = c(25,5), density.info = "none",
-	  trace = "none", cexRow = 0.7,
-	  srtCol = 45, cexCol = 0.6)
+tab_p <- round(tab, digits = 2)
+tab_p[nonsig] <- NA
+
+pdf("heatmap.pdf", width = 50, height = 50)
+heatmap.2(tab, cellnote = tab_p, notecex = 4.0, notecol = "black",
+	  col = redblue(75), margins = c(25, 120), density.info = "none", trace = "none",
+	  cexRow = 4.0, srtCol = 45, cexCol = 4.0,
+	  lhei = c(1, 8), lwid = c(1.2, 4), Colv = FALSE, dendrogram = "row")
 dev.off()
