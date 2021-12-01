fd28e453390dae6096d5b5cc49a4a719a155b716
TiysEvgeny <tiys@ngs.ru>
2021-12-01 18:54:54 +0700

    Remove  variant of SMR-HEIDI using GWAS-MAP

--- 03_sh_ma_func_an/smr-heidi/03_smr-heidi_report.sh
+++
@@ -1,9 +0,0 @@
-# Aim of this script is obtain SMR-HEIDI report for SH GWAS
-
-run_smrheidi_report \
-        --gwas-id-1 2269986 \
-        --output-dir /mnt/polyomica/projects/bp-sh/data/03_sh_ma_func_an/smr-heidi/ \
-        --version 1 \
-        --output-file sh_smr-heidi #\
-#        --only-table
-
