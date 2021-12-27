105e756b4db4f073e1f1989afaae3fbd616f26c9
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2021-12-27 20:28:29 +0700

    Added scripts filtering SMR-HEIDI results by tissues

---
+++ 03_sh_ma_func_an/smr-heidi/04_filter_smr_heidi_results.R
@@ -0,0 +1,64 @@
+# Aim of this script is to filter the results of SMR-HEIDI analysis for SH
+
+library(data.table)
+
+setwd("/mnt/polyomica/projects/bp-sh/data/03_sh_ma_func_an/smr-heidi")
+
+# Read a file with preselected tissue types that are needed
+tissues <- fread("matching_tissues.txt", data.table = F)
+
+# Read the SMR-HEIDI results
+gtex <- fread("gtex_v7/results.tsv", data.table = F)
+dim(gtex)
+cedar <- fread("cedar/results.tsv", data.table = F)
+dim(cedar)
+westra <- fread("westra/results.tsv", data.table = F)
+
+# A function to extract certain tissue types from SMR-HEIDI reults using predefined list of tissues
+filter_smar_heidi_results <- function(tissues, smr_heidi_res){
+	matches <- lapply(tissues, function(x) grep(x, smr_heidi_res$gwas_2))
+	matches <- unlist(matches)
+	filterted <- smr_heidi_res[matches, ]
+	return(filterted)
+}
+
+# Filter GTEX data
+gtex_f <- filter_smar_heidi_results(tissues = tissues$tissue_name[tissues$Dataset == "GTEX_v7" & !is.na(tissues$tissue_name)], smr_heidi_res = gtex) 
+gtex_f$Source <- "GTEx_v7"
+
+# Filter CEDAR data
+cedar_f <- filter_smar_heidi_results(tissues = tissues$tissue_name[tissues$Dataset == "cedar" & !is.na(tissues$tissue_name)], smr_heidi_res = cedar)
+cedar_f$Source <- "CEDAR"
+
+# Westra should not be filtered
+westra$Source <- "Westra"
+
+# Merge and save
+# Check columns
+length(colnames(gtex_f)) # 53
+length(colnames(cedar_f)) # 52
+length(colnames(westra)) # 53
+which(!(colnames(gtex_f) %in% colnames(cedar_f))) # 46
+colnames(gtex_f)[46] # maf_G2
+table(colnames(gtex_f)[-46] == colnames(cedar_f)) # all true
+table(colnames(gtex_f) == colnames(westra)) # all true
+# can ignor maf_G2 column
+merged_full <- rbind(gtex_f[, -c(46)], cedar_f)
+merged_full <- rbind(merged_full, westra[, -c(46)])
+fwrite(merged_full, file = "sh_results_full_preselected_tissues.txt", dec = ".", sep = "\t")
+
+# Let's filter the columns
+merged_filtered <- merged_full[, c("locus", "rsid_REF", "locus_top_ld_r", "gwas_1", "gwas_2", "Source", "gene_name_G2", "beta_smr", "sigma_smr", "p_smr", "p_heidi", "n_heidi", "p_G2", "probe_G2")]
+fwrite(merged_filtered, file = "sh_results_filtered_columns.txt", dec = ".", sep = "\t")
+
+# Let's filter the SNPs. Select only replicated
+merged_fin <- merged_filtered[merged_filtered$locus == "rs2587363" | merged_filtered$locus == "rs11079993", ]
+fwrite(merged_fin, file = "sh_results_repl_only.txt", dec = ".", sep = "\t")
+# set a p_smr threshold: 0.05/N, where N is the number of unique combinations of tissue-probe for the two replicated SNPs
+thr_smr <- 0.05/length(unique(merged_fin$gwas_2))
+thr_heidi <- 0.01 # standard threshold
+table(merged_fin$p_smr < thr_smr)
+table(merged_fin$p_heidi >= thr_heidi)
+table(merged_fin$p_heidi >= thr_heidi & merged_fin$p_smr < thr_smr)
+ind <- which(merged_fin$p_heidi >= thr_heidi & merged_fin$p_smr < thr_smr)
+fwrite(merged_fin[ind, ], file = "sh_results_passed_thr.txt", dec = ".", sep = "\t")
