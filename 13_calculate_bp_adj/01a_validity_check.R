1628a20d7e049bc0c5c68261f6ca14c7197229d7
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2022-03-03 23:03:09 +0700

    Minor changes

--- 13_calculate_bp_adj/01a_validity_check.R
+++ 13_calculate_bp_adj/01a_validity_check.R
@@ -1,5 +1,5 @@
-# Aim of this script is to estimate heritability of adjusted BP-SH
-# and its genetic correlation with shared heredity, back pain, BP-SH, CKP and CHP 
+# Aim of this script is to estimate heritability of 
+# BP adjusted for all other original pain traits 
 
 library(data.table)
 
@@ -14,7 +14,7 @@ source("../../shared_heredity/00_core_functions/cor_g_a.R")
 alphas <- read.table('../../../data/01_sh/alphas.txt', row.names = 1)
 alphas <- as.numeric(alphas[2, ])
 
-# coefficients for adjusted BP-SH
+# coefficients for adjusted BP
 aa <- c(-0.071018123, 0.854891273, -0.790903139, 0.017435276, 0.001350986, -0.067428072) # "Hip", "Back", "Neck", "Knee", "Head", "Stomach"
 
 n_traits <- c(1:length(alphas))
@@ -30,22 +30,22 @@ position <- diag(length(alphas))
 H2(aa, covm = gcov, phem = phem)
 # [1] 0.007168787
 
-# Estimate pairwise genetic correlations for adjusted BP-SH and other traits
+# Estimate pairwise genetic correlations for adjusted BP and other traits
 cor_gi_a1_a2(a1 = alphas, a2 = aa, covm = gcov[c(1:6), c(1:6)]) 
-#[1] 0.151285 rg for adjusted bp-sh and sh
+#[1] 0.151285 rg for adjusted bp and sh
 cor_gi_a1_a2(a1 = position[2, ] - alphas*slope[2], a2 = aa, covm = gcov[c(1:6), c(1:6)])
-#[1] 0.8021326 rg for adjusted bp-sh and bp-sh
+#[1] 0.8021326 rg for adjusted bp and bp-sh
 tmp <- lapply(n_traits, function(x) cor_gi_alfa(i = n_traits[x], a = aa, covm = gcov))
 #[[1]]
-#[1] 3.604807e-11 rg for adjusted bp-sh and hip pain
+#[1] 3.604807e-11 rg for adjusted bp and hip pain
 #[[2]]
-#[1] 0.4823323 rg for adjusted bp-sh and back pain
+#[1] 0.4823323 rg for adjusted bp and back pain
 #[[3]]
-#[1] -2.194532e-10 rg for adjusted bp-sh and neck pain
+#[1] -2.194532e-10 rg for adjusted bp and neck pain
 #[[4]]
-#[1] 1.845365e-10 rg for adjusted bp-sh and knee pain
+#[1] 1.845365e-10 rg for adjusted bp and knee pain
 #[[5]]
-#[1] 2.357728e-10 rg for adjusted bp-sh and head pain
+#[1] 2.357728e-10 rg for adjusted bp and head pain
 #[[6]]
-#[1] 7.117874e-11 rg for adjusted bp-sh and stomach pain
+#[1] 7.117874e-11 rg for adjusted bp and stomach pain
 
