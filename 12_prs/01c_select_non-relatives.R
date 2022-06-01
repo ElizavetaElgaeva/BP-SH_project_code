123c88990452a546a2b86546357ebde6b2082587
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2022-06-01 11:55:30 +0700

    Extract non-relatives from test sample from ICD10 code level 2 data; filter level 2 codes by prevalence

--- 12_prs/01c_select_non-relatives.R
+++ 12_prs/01c_select_non-relatives.R
@@ -1,4 +1,5 @@
 # Aim of this script is to select non-relatieves from the test sampe for PRSanalysis
+# and filter data by prevalence
 
 library(data.table)
 setwd("/mnt/polyomica/projects/bp-sh/data/12_prs")
@@ -21,22 +22,32 @@ length(inter) # 120217
 setwd("/home/common/projects/pain_project/UKBB_pheno_ICD10_OPCS")
 
 load("./bp_prs_iid_icd10_filtered.RData") # BP data
-load("./icd10_iid_cbp_prev_filtered.RData") # ICD10 data
-load("./opcs_iid_cbp_prev_filtered.RData") # OPCS data
+load("./icd10_iid_level_2_cbp_filtered.RData") # ICD10 data
+#load("./opcs_iid_cbp_prev_filtered.RData") # OPCS data
 
 ls()
 
 table(inter %in% bp_f$IID)
-table(inter %in% icd_f_prev$IID)
-table(inter %in% opcs_f_prev$IID)
+table(inter %in% icd_f_l2$IID)
+#table(inter %in% opcs_f_prev$IID)
 
-# IID order is the same in bp_f, icd_f_prev, opcs_f_prev
+# IID order is the same in bp_f, icd_f_l2, opcs_f_prev
 i <- which(bp_f$IID %in% inter)
 bp_f_test_nonr <- bp_f[i, ]
-icd_f_prev_test_nonr <- icd_f_prev[i, ]
-opcs_f_prev_test_nonr <- opcs_f_prev[i, ]
+icd_f_l2_test_nonr <- icd_f_l2[i, ]
+#opcs_f_prev_test_nonr <- opcs_f_prev[i, ]
 
+# Filter ICD10 codes by prevalence
+prev <- lapply(icd_f_l2_test_nonr[, -c(1:22)], function(x) sum(x)) # the first 22 columns contain no ICD10 codes
+prev <- unlist(prev)
+n <- nrow(icd_f_l2_test_nonr)
+prev <- lapply(prev, function(x) x/n*100)
+ind_prev <- which(prev > 0.5 & prev < 99.5) # 245 ICD10 codes meet the criteria
+icd_f_l2_test_nonr_prev <- icd_f_l2_test_nonr[, c(1:22, ind_prev + 22)] # need to shift the indexes since the were defined in data without first 22 columns
+prev_icd_f <- prev[ind_prev]
+
+# Save data
 save(bp_f_test_nonr, file = "bp_prs_iid_icd10_filtered_test_nonrelatives.RData")
-save(icd_f_prev_test_nonr, file = "icd10_iid_cbp_prev_filtered_test_nonrelatives.RData")
-save(opcs_f_prev_test_nonr, file = "opcs_iid_cbp_prev_filtered_test_nonrelatives.RData")
+save(icd_f_l2_test_nonr, file = "icd10_iid_level_2_cbp_prev_filtered_test_nonrelatives.RData")
+#save(opcs_f_prev_test_nonr, file = "opcs_iid_cbp_prev_filtered_test_nonrelatives.RData")
 
