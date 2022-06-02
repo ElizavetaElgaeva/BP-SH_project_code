67a19479fc194923493ebb640693ebb3869cfcfd
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2022-06-02 16:43:06 +0700

    Rerun analyses for codes combined to level 2 (ICD10 codes from I-XVII chapters only; OPCS codes without chapters X, Y, Z

--- 12_prs/07_v2_hclust.R
+++ 12_prs/07_v2_hclust.R
@@ -1,74 +1,83 @@
 # Aim of this script is to perform clustering of the ICD10 and OPS traits
-# (Test sample, non-relatieves only; standardized PRS)
+# (Test sample, non-relatieves only; standardized PRS; codes combined to level 2;
+# ICD10 codes from chapters I-XVII only, OPCS codes without X, Y, Z chapter)
 
 setwd("/home/common/projects/pain_project/UKBB_pheno_ICD10_OPCS/")
 
 # Load tables of ICD10 and OPCS codes
-load("./icd10_iid_cbp_prev_filtered_test_nonrelatives.RData")
-load("./opcs_iid_cbp_prev_filtered_test_nonrelatives.RData")
+load("./icd10_iid_level_2_cbp_prev_filtered_test_nonrelatives.RData")
+load("./opcs_iid_level_2_cbp_prev_filtered_test_nonrelatives.RData")
 
 setwd("/mnt/polyomica/projects/bp-sh/data/12_prs")
 
 # Load glm() results
 # BP PRS
-load("./glm_icd_bp_prs_filtered_test_nonrelatives.RData")
-load("./glm_opcs_bp_prs_filtered_test_nonrelatives.RData")
+load("./glm_icd_level_2_chapter_1-17_bp_prs_filtered_test_nonrelatives.RData")
+load("./glm_opcs_level_2_no_xyz_bp_prs_filtered_test_nonrelatives.RData")
 
 # SH PRS
-load("./glm_icd_sh_prs_filtered_test_nonrelatives.RData")
-load("./glm_opcs_sh_prs_filtered_test_nonrelatives.RData")
+load("./glm_icd_level_2_chapter_1-17_sh_prs_filtered_test_nonrelatives.RData")
+load("./glm_opcs_level_2_no_xyz_sh_prs_filtered_test_nonrelatives.RData")
 
 # BP-SH PRS
-load("./glm_icd_bp_sh_prs_filtered_test_nonrelatives.RData")
+load("./glm_opcs_level_2_no_xyz_bp_sh_prs_filtered_test_nonrelatives.RData")
 
 ls()
 
 # Select ICD10 and OPCS codes for clustering
 codes <- names(bp_icd)
 codes <- c(codes, names(sh_icd))
-codes <- c(codes, names(bp_sh_icd))
 codes <- unique(codes)
 
 codes2 <- names(bp_opcs)
 codes2 <- c(codes2, names(sh_opcs))
+codes2 <- c(codes2, names(bp_sh_opcs))
 codes2 <- unique(codes2)
 
-intersect(codes, codes2) # null
+intersect(codes, codes2) # "E03" "K63" "M47" "J18"
 all_codes <- c(codes, codes2)
 
 # Calculate phenotypic correlation matrix between ICD10 and OPCS codes
-icd_ind <- na.omit(match(codes, colnames(icd_f_prev_test_nonr)))
-opcs_ind <- na.omit(match(codes2, colnames(opcs_f_prev_test_nonr)))
-pheno_joint <- cbind(icd_f_prev_test_nonr[, icd_ind], opcs_f_prev_test_nonr[, opcs_ind])
+icd_ind <- na.omit(match(codes, colnames(icd_f_l2_test_nonr_prev)))
+opcs_ind <- na.omit(match(codes2, colnames(opcs_f_l2_test_nonr_prev)))
+pheno_joint <- cbind(icd_f_l2_test_nonr_prev[, icd_ind], opcs_f_l2_test_nonr_prev[, opcs_ind])
 pheno_cor <- cor(pheno_joint)
 
 # Perform hierarchical clustering
 dd <- (1 - abs(pheno_cor)) # transform to distance matrix
 
+colnames(dd)[1:92] <- rownames(dd)[1:92] <- paste0(colnames(dd)[1:92], "_ICD10")
+colnames(dd)[93:149] <- rownames(dd)[93:149] <- paste0(colnames(dd)[93:149], "_OPCS4")
+
 d <- as.dist(dd)
 h1 <- hclust(d, method = "ward.D2")
-results <- data.table::fread("glm_results_extended_desc_test_nonrelatives.txt", data.table = F)
-table(h1$labels %in% results$code) # all true
-j <- match(h1$labels, results$code)
-table(results$code[j] == h1$labels) # all true
+results <- data.table::fread("glm_results_level_2_preselected_codes_extended_desc_test_nonrelatives.txt", data.table = F)
+table(substr(h1$labels, 1, 3) %in% results$code) # all true
+table(substr(h1$labels, 1, 3) == results$code[1:149]) # all true
+j <- c(1:length(h1$labels))
+table(results$code[j] == substr(h1$labels, 1, 3)) # all true
 h1$labels <- results$description[j]
 
-pdf("hclust_test_nonrelatives.pdf", width = 30, height = 30)
+pdf("hclust_level_2_test_nonrelatives.pdf", width = 30, height = 30)
 plot(h1, hang = -1)
 abline(h = 1.5, col = "blue")
-abline(h = 1.4, col = "red")
-abline(h = 1.3, col = "green")
+abline(h = 1.35, col = "red")
+abline(h = 1.25, col = "green")
 dev.off()
 
-h2 <- cutree(tree = h1, h = 1.5)
-save(h2, file = "hclust_1.5.RData")
+h2 <- cutree(tree = h1, h = 1.25)
+save(h2, file = "hclust_level_2_thr_1.25.RData")
+
+h3 <- cutree(tree = h1, h = 1.35)
+save(h3, file = "hclust_level_2_thr_1.35.RData")
 
 # Heatmap visualization
 library(corrplot)
 
+colnames(pheno_cor) <- rownames(pheno_cor) <- colnames(dd)
 rg2plot <- pheno_cor[h1$order, h1$order]
 
-pdf("heatmap_hclust_test_nonrelatieves.pdf", width = 30, height = 30)
+pdf("heatmap_hclust_lenel_2_test_nonrelatieves.pdf", width = 30, height = 30)
 corrplot(as.matrix(rg2plot), type = 'full', is.corr = T)
 dev.off()
 
--- 12_prs/07_v3_heatmap.R
+++ 12_prs/07_v3_heatmap.R
@@ -1,5 +1,5 @@
 # Aim of this script is to create heatmap plot for glm() results for ICD10 and OPCS codes
-# representing clusters (h = 1.25) against standardized PRS, test sample, non-relatives
+# representing clusters (h = 1.35) against standardized PRS, test sample, non-relatives
 # Note: all codes combined to level 2; ICD10 codes from chapters I-XVII only; OPCS codes excluding X, Y, Z chapters
 
 library(data.table)
@@ -30,137 +30,119 @@ colnames(tab) <- c("CBP PRS", "SGIT PRS", "CBP UGIT PRS")
 #tab_p[nonsig] <- NA
 
 # Load cluster data
-load("hclust_level_2_thr_1.25.RData")
+load("hclust_level_2_thr_1.35.RData")
 
 # Select the most representative traits from clusters
-table(names(h2) %in% rownames(tab))
-which(!(rownames(tab) %in% names(h2)))
+table(names(h3) %in% rownames(tab))
+which(!(rownames(tab) %in% names(h3)))
 #150 151 152 153 154 155
 rownames(tab)[c(150, 151, 152, 153, 154, 155)]
-table(names(h2) == rownames(tab)[-c(150, 151, 152, 153, 154, 155)]) #all true
-table(h2)
-#h2
-# 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19
-# 6  4 28 14 10  4  8  8 11  3  7  4 14  7  4  4  7  4  2
-h2 <- sort(h2, decreasing = FALSE)
-tab_cut <- tab[names(h2), ]
-pp_cut <- pp[names(h2), ]
+table(names(h3) == rownames(tab)[-c(150, 151, 152, 153, 154, 155)]) #all true
+table(h3)
+#h3
+# 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16
+# 6 15 34 14 10  4  8  8  3  7  4 14  7  4  4  7
+h3 <- sort(h3, decreasing = FALSE)
+tab_cut <- tab[names(h3), ]
+pp_cut <- pp[names(h3), ]
 
-c1 <- which(h2 == 1)
+c1 <- which(h3 == 1)
 min(pp_cut[c1, ])
 table(pp_cut == min(pp_cut[c1, ])) # unique
 i1 <- which(pp_cut == min(pp_cut[c1, ]))
 traits <- c(rownames(which(pp_cut == pp_cut[i1], arr.ind = TRUE))) # trait from the 1st cluster
 
-c2 <- which(h2 == 2)
+c2 <- which(h3 == 2)
 min(pp_cut[c2, ])
 table(pp_cut == min(pp_cut[c2, ])) # unique
 i2 <- which(pp_cut == min(pp_cut[c2, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i2], arr.ind = TRUE))) # add trait from the 2nd cluster
 
-c3 <- which(h2 == 3)
+c3 <- which(h3 == 3)
 min(pp_cut[c3, ])
 table(pp_cut == min(pp_cut[c3, ])) # unique
 i3 <- which(pp_cut == min(pp_cut[c3, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i3], arr.ind = TRUE))) # add trait from the 3d cluster
 
-c4 <- which(h2 == 4)
+c4 <- which(h3 == 4)
 min(pp_cut[c4, ])
 table(pp_cut == min(pp_cut[c4, ])) # unique
 i4 <- which(pp_cut == min(pp_cut[c4, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i4], arr.ind = TRUE))) # add trait from the 4th cluster
 
-c5 <- which(h2 == 5)
+c5 <- which(h3 == 5)
 min(pp_cut[c5, ])
 table(pp_cut == min(pp_cut[c5, ])) # unique
 i5 <- which(pp_cut == min(pp_cut[c5, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i5], arr.ind = TRUE))) # add trait from the 5th cluster
 
-c6 <- which(h2 == 6)
+c6 <- which(h3 == 6)
 min(pp_cut[c6, ])
 table(pp_cut == min(pp_cut[c6, ])) # unique
 i6 <- which(pp_cut == min(pp_cut[c6, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i6], arr.ind = TRUE))) # add trait from the 6th cluster
 
-c7 <- which(h2 == 7)
+c7 <- which(h3 == 7)
 min(pp_cut[c7, ])
 table(pp_cut == min(pp_cut[c7, ])) # unique
 i7 <- which(pp_cut == min(pp_cut[c7, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i7], arr.ind = TRUE)))# add trait from the 7th cluster
 
-c8 <- which(h2 == 8)
+c8 <- which(h3 == 8)
 min(pp_cut[c8, ])
 table(pp_cut == min(pp_cut[c8, ])) # unique
 i8 <- which(pp_cut == min(pp_cut[c8, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i8], arr.ind = TRUE)))# add trait from the 8th cluster
 
-c9 <- which(h2 == 9)
+c9 <- which(h3 == 9)
 min(pp_cut[c9, ])
 table(pp_cut == min(pp_cut[c9, ])) # unique
 i9 <- which(pp_cut == min(pp_cut[c9, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i9], arr.ind = TRUE))) # add trait from the 9th cluster
 
-c10 <- which(h2 == 10)
+c10 <- which(h3 == 10)
 min(pp_cut[c10, ])
 table(pp_cut == min(pp_cut[c10, ])) # unique
 i10 <- which(pp_cut == min(pp_cut[c10, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i10], arr.ind = TRUE))) # add trait from the 10th cluster
 
-c11 <- which(h2 == 11)
+c11 <- which(h3 == 11)
 min(pp_cut[c11, ])
 table(pp_cut == min(pp_cut[c11, ])) # unique
 i11 <- which(pp_cut == min(pp_cut[c11, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i11], arr.ind = TRUE))) # add trait from the 11th cluster
 
-c12 <- which(h2 == 12)
+c12 <- which(h3 == 12)
 min(pp_cut[c12, ])
 table(pp_cut == min(pp_cut[c12, ])) # unique
 i12 <- which(pp_cut == min(pp_cut[c12, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i12], arr.ind = TRUE))) # add trait from the 12th cluster
 
-c13 <- which(h2 == 13)
+c13 <- which(h3 == 13)
 min(pp_cut[c13, ])
 table(pp_cut == min(pp_cut[c13, ])) # unique
 i13 <- which(pp_cut == min(pp_cut[c13, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i13], arr.ind = TRUE))) # add trait from the 13th cluster
 
-c14 <- which(h2 == 14)
+c14 <- which(h3 == 14)
 min(pp_cut[c14, ])
 table(pp_cut == min(pp_cut[c14, ])) # unique
 i14 <- which(pp_cut == min(pp_cut[c14, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i14], arr.ind = TRUE))) # add trait from the 14th cluster
 
-c15 <- which(h2 == 15)
+c15 <- which(h3 == 15)
 min(pp_cut[c15, ])
 table(pp_cut == min(pp_cut[c15, ])) # unique
 i15 <- which(pp_cut == min(pp_cut[c15, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i15], arr.ind = TRUE))) # add trait from the 15th cluster
 
-c16 <- which(h2 == 16)
+c16 <- which(h3 == 16)
 min(pp_cut[c16, ])
 table(pp_cut == min(pp_cut[c16, ])) # unique
 i16 <- which(pp_cut == min(pp_cut[c16, ]))
 traits <- c(traits, rownames(which(pp_cut == pp_cut[i16], arr.ind = TRUE))) # add trait from the 16th cluster
 
-c17 <- which(h2 == 17)
-min(pp_cut[c17, ])
-table(pp_cut == min(pp_cut[c17, ])) # unique
-i17 <- which(pp_cut == min(pp_cut[c17, ]))
-traits <- c(traits, rownames(which(pp_cut == pp_cut[i17], arr.ind = TRUE))) # add trait from the 17th cluster
-
-c18 <- which(h2 == 18)
-min(pp_cut[c18, ])
-table(pp_cut == min(pp_cut[c18, ])) # unique
-i18 <- which(pp_cut == min(pp_cut[c18, ]))
-traits <- c(traits, rownames(which(pp_cut == pp_cut[i18], arr.ind = TRUE))) # add trait from the 18th cluster
-
-c19 <- which(h2 == 19)
-min(pp_cut[c19, ])
-table(pp_cut == min(pp_cut[c19, ])) # unique
-i19 <- which(pp_cut == min(pp_cut[c19, ]))
-traits <- c(traits, rownames(which(pp_cut == pp_cut[i19], arr.ind = TRUE))) # add trait from the 19th cluster
-
-h2[traits] # self-check, each cluster is represented
+h3[traits] # self-check, each cluster is represented
 tab_cut <- tab_cut[traits, ]
 pp_cut <- pp_cut[traits, ]
 
@@ -176,11 +158,11 @@ tab_fin <- round(tab_fin, digits = 2)
 notsig <- which(pp_fin > thr)
 pp_fin[notsig] <- NA
 
-pdf("heatmap_level_2_thr_1.25_test_nonrel.pdf", width = 50, height = 50)
+pdf("heatmap_level_2_thr_1.35_test_nonrel.pdf", width = 50, height = 50)
 heatmap.2(tab_fin, cellnote = tab_fin, notecex = 4.0, notecol = "black", na.color = "grey",
           col = redblue(75), margins = c(25, 120), density.info = "none", trace = "none",
           cexRow = 4.0, srtCol = 45, cexCol = 4.0,
           lhei = c(1, 8), lwid = c(1.2, 4), Colv = FALSE, dendrogram = "row")
 dev.off()
 
-save(traits, tab_fin, pp_fin, file = "heatmap_level_2_clusters_1.25.RData")
+save(traits, tab_fin, pp_fin, file = "heatmap_level_2_clusters_1.35.RData")
