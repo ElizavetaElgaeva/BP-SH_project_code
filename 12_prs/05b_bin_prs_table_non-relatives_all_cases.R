a62636226aa2cbee3a7c5fb39973ded6ae2fe009
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2022-07-20 19:48:51 +0700

    Added script creating table with glm() results

---
+++ 12_prs/05b_bin_prs_table_non-relatives_all_cases.R
@@ -0,0 +1,113 @@
+# Aim of this script is to create a table with glm() results for binary coded SH/BP-SH PRS (non-overlapping 0.1 and 0.9 quantiles)
+# (Whole sample, cases, non-relatieves only; standardized PRS; ICD10 and OPCS codes not-combined)
+# Note: ICD10 from chapter I - XVII only; X, Y, Z OPCS codes excluded
+
+library(data.table)
+library(dplyr)
+
+setwd("/mnt/polyomica/projects/bp-sh/data/12_prs")
+
+# Load full glm() results
+# SH
+load("icd10_chapter1-17_vs_bin_prs_10_sh_all_cases.RData")
+load("icd10_chapter1-17vs_bin_sh_prs_90_sh_all_cases.RData")
+
+load("opcs_no_xyz_vs_bin_prs_10_sh_all_cases.RData")
+load("opcs_no_xyz_vs_bin_prs_90_sh_all_cases.RData")
+
+# BP-SH
+load("icd10_chapter1-17_vs_bin_prs_10_bp_sh_all_cases.RData")
+load("icd10_chapter1-17_vs_bin_prs_90_bp_sh_all_cases.RData")
+
+load("opcs_no_xyz_bin_prs_10_bp_sh_all_cases.RData")
+load("opcs_no_xyz_vs_bin_prs_90_bp_sh_all_cases.RData")
+
+
+# Load glm results filtered by statistical significance
+# SH PRS
+load("icd_ch1-17_vs_10_sh_all_nonr_cases_signif.RData")
+load("icd_ch1-17_vs_90_sh_all_nonr_cases_signif.RData")
+
+load("opcs_no_xyz_vs_10_sh_all_nonr_cases_signif.RData")
+load("opcs_no_xyz_vs_90_sh_all_nonr_cases_signif.RData")
+
+# BP-SH PRS
+load("icd_ch1-17_vs_10_bp_sh_all_nonr_cases_signif.RData")
+load("icd_ch1-17_vs_90_bp_sh_all_nonr_cases_signif.RData")
+
+load("opcs_no_xyz_vs_10_bp_sh_all_nonr_cases_signif.RData")
+load("opcs_no_xyz_vs_90_bp_sh_all_nonr_cases_signif.RData")
+
+ls()
+
+# Select ICD10 and OPCS codes of interest
+codes <- rownames(icd_vs_10_sh_sign)
+codes <- c(codes, rownames(icd_vs_90_sh_sign))
+codes <- c(codes, rownames(icd_vs_10_bp_sh_sign))
+codes <- c(codes, rownames(icd_vs_90_bp_sh_sign))
+codes <- unique(codes)
+
+codes2 <- rownames(opcs_vs_10_sh_sign)
+codes2 <- c(codes2, rownames(opcs_vs_90_sh_sign))
+codes2 <- c(codes2, rownames(opcs_vs_10_bp_sh_sign))
+codes2 <- c(codes2, rownames(opcs_vs_90_bp_sh_sign))
+codes2 <- unique(codes2)
+
+intersect(codes, codes2) # 0
+all_codes <- c(codes, codes2) # length(codes) = 84, length(codes2) = 34
+icd_ind <- c(1:length(codes))
+
+# Create a table
+tab <- as.data.frame(matrix(ncol = 20, nrow = length(all_codes)))
+colnames(tab) <- c("nomenclature", "code", "description", "10_sh_or", "10_sh_beta", "10_sh_se", "10_sh_p", "90_sh_or", "90_sh_beta", "90_sh_se", "90_sh_p", "10_bp_sh_or", "10_bp_sh_beta", "10_bp_sh_se", "10_bp_sh_p", "90_bp_sh_or", "90_bp_sh_beta", "90_bp_sh_se", "90_bp_sh_p", "code_prevalence")
+tab$code <- all_codes
+tab$nomenclature[icd_ind] <- "ICD10"
+tab$nomenclature[-icd_ind] <- "OPCS4"
+
+# Add description
+icd_des <- readxl::read_excel("/home/common/projects/pain_project/UKBB_pheno_ICD10_OPCS/ICD10_coding19.xlsx", sheet = 1)
+opcs_des <- readxl::read_excel("/home/common/projects/pain_project/UKBB_pheno_ICD10_OPCS/OPCS_coding240.xlsx", sheet = 1)
+
+i_icd_des <- match(codes, icd_des$"coding")
+i_opcs_des <- match(codes2, opcs_des$"coding")
+
+icd_des <- icd_des[i_icd_des, c("coding", "meaning")]
+opcs_des <- opcs_des[i_opcs_des, c("coding", "meaning")]
+icd_opcs_des <- rbind(icd_des, opcs_des)
+table(tab$code == icd_opcs_des$"coding")
+tab$description <- icd_opcs_des$"meaning"
+
+
+sh_10_full <- c(if_10_sh_vs_icd[codes], if_10_sh_vs_opcs[codes2])
+tab$"10_sh_or" <- sapply(sh_10_full, function(x) exp(x[, 1]))
+tab$"10_sh_beta" <- sapply(sh_10_full, function(x) x[, 1])
+tab$"10_sh_se" <- sapply(sh_10_full, function(x) x[, 2])
+tab$"10_sh_p" <- sapply(sh_10_full, function(x) x[, 4])
+
+sh_90_full <- c(if_90_sh_vs_icd[codes], if_90_sh_vs_opcs[codes2])
+tab$"90_sh_or" <- sapply(sh_90_full, function(x) exp(x[, 1]))
+tab$"90_sh_beta" <- sapply(sh_90_full, function(x) x[, 1])
+tab$"90_sh_se" <- sapply(sh_90_full, function(x) x[, 2])
+tab$"90_sh_p" <- sapply(sh_90_full, function(x) x[, 4])
+
+bp_sh_10_full <- c(if_10_bp_sh_vs_icd[codes], if_10_bp_sh_vs_opcs[codes2])
+tab$"10_bp_sh_or" <- sapply(bp_sh_10_full, function(x) exp(x[, 1]))
+tab$"10_bp_sh_beta" <- sapply(bp_sh_10_full, function(x) x[, 1])
+tab$"10_bp_sh_se" <- sapply(bp_sh_10_full, function(x) x[, 2])
+tab$"10_bp_sh_p" <- sapply(bp_sh_10_full, function(x) x[, 4])
+
+bp_sh_90_full <- c(if_90_bp_sh_vs_icd[codes], if_90_bp_sh_vs_opcs[codes2])
+tab$"90_bp_sh_or" <- sapply(bp_sh_90_full, function(x) exp(x[, 1]))
+tab$"90_bp_sh_beta" <- sapply(bp_sh_90_full, function(x) x[, 1])
+tab$"90_bp_sh_se" <- sapply(bp_sh_90_full, function(x) x[, 2])
+tab$"90_bp_sh_p" <- sapply(bp_sh_90_full, function(x) x[, 4])
+
+
+load("//home/common/projects/pain_project/UKBB_pheno_ICD10_OPCS/prev_of_icd10_cases_all_nonrel_filtered.RData")
+load("/home/common/projects/pain_project/UKBB_pheno_ICD10_OPCS/prev_opcs_cases_all_nonrel_filtered.RData")
+prev_full <- c(prev_icd_cases_f[codes], prev_opcs_cases_f[codes2])
+tab$"code_prevalence" <- prev_full
+
+fwrite(tab, file = "glm_results_preselected_codes_nonrel_all_cases.txt", sep = "\t", dec = ".")
+
+
